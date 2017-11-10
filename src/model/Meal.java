d0834746ddf72afc1f27f2e891f88444610bfd2d
Nixon <nixon_ong@dlsu.edu.ph>
2017-11-10 23:25:59 +0800

    dbmodel & meal

--- src/model/Meal.java
+++ src/model/Meal.java
@@ -3,17 +3,32 @@ package model;
 import java.util.*;
 
 public class Meal {
+    private int mealID;
     private ArrayList<ConsumableQuantityPair> addOns;
     
-    public Meal(ArrayList<ConsumableQuantityPair> addOns) {
+    public Meal(int mealID, ArrayList<ConsumableQuantityPair> addOns) 
+    {
+        this.mealID = mealID;
         this.addOns = addOns;
     }
 
-    public ArrayList<ConsumableQuantityPair> getAddOns() {
+    public ArrayList<ConsumableQuantityPair> getAddOns() 
+    {
         return addOns;
     }
 
-    public void setAddOns(ArrayList<ConsumableQuantityPair> addOns) {
+    public void setAddOns(ArrayList<ConsumableQuantityPair> addOns) 
+    {
         this.addOns = addOns;
+    }
+
+    public int getMealID()
+    {
+        return mealID;
+    }
+
+    public void setMealID(int mealID)
+    {
+        this.mealID = mealID;
     }
 }
