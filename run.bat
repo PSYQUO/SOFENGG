24947405088ed28ed4b7498adddc6368c3de50a8
alvinalvord <jasper_pillejera@dlsu.edu.ph>
2017-11-22 21:03:19 +0800

    update

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-javac -d bin src/*.java src/view/*.java src/receipt/*.java src/model/*.java src/model/transaction/*.java src/controller/*.java src/controller/ViewManager/*.java
+javac -d bin -Xlint:all -Xdiags:verbose src/*.java src/view/*.java src/receipt/*.java src/model/*.java src/model/transaction/*.java src/controller/*.java src/controller/ViewManager/*.java
 
 cd bin
 java App
