ff66834d1349d3c617b7977ed1927c0e96ddc02d
Patrick Sy <patrick.alq.sy@gmail.com>
2017-11-24 23:10:27 +0800

    Add Inventory Functions and Refactor InventoryController (#52)
    * Implement proper TableView population
    
    Add Items to table view using RawItem object
    
    * Add add and subtract quantity function to invetory
    
    * Add todos to button functions

--- src/controller/InventoryController.java
+++ src/controller/InventoryController.java
@@ -8,24 +8,30 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import model.*;
-
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Callback;
+import model.DatabaseModel;
+import model.RawItem;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class InventoryController extends Controller
 {
     @FXML
-    private TableView tableviewInventory;
+    private TableView<RawItem> tableviewInventory;
 
     @FXML
-    private TableColumn colIngredients, colNumber;
+    private TableColumn<RawItem, String> colIngredients;
 
     @FXML
-    private Button buttonBack;
+    private TableColumn<RawItem, Integer> colNumber;
+
+    @FXML
+    private Button buttonBack, greenBut, redBut;
 
     public InventoryController() throws IOException
     {
@@ -37,15 +43,38 @@ public class InventoryController extends Controller
     {
         if(checkInitialLoad(getClass().getSimpleName()))
         {
-            colIngredients.setCellValueFactory(
-                    (Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>)
-                            param -> new SimpleStringProperty(param.getValue().get(0).toString()));
-
-            colNumber.setCellValueFactory(
-                    (Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>)
-                            param -> new SimpleStringProperty(param.getValue().get(1).toString()));
+            colIngredients.setCellValueFactory(new PropertyValueFactory<>("name"));
+            colNumber.setCellValueFactory(new PropertyValueFactory<>("quantity"));
 
             loadRawItems();
+
+            greenBut.addEventHandler(ActionEvent.ACTION, e ->
+            {
+                RawItem selectedItem = tableviewInventory.getSelectionModel().getSelectedItem();
+                if(selectedItem != null)
+                {
+                    selectedItem.setQuantity(selectedItem.getQuantity() + 1);
+                    tableviewInventory.refresh();
+
+                    /**
+                     * TODO: Update DB with RawItem added quantity
+                     */
+                }
+            });
+
+            redBut.addEventHandler(ActionEvent.ACTION, e ->
+            {
+                RawItem selectedItem = tableviewInventory.getSelectionModel().getSelectedItem();
+                if(selectedItem != null)
+                {
+                    selectedItem.setQuantity(selectedItem.getQuantity() - 1);
+                    tableviewInventory.refresh();
+
+                    /**
+                     * TODO: Update DB with RawItem subtracted quantity
+                     */
+                }
+            });
 
             buttonBack.addEventHandler(ActionEvent.ACTION, e ->
             {
@@ -65,18 +94,9 @@ public class InventoryController extends Controller
     {
         DatabaseModel dbm = new DatabaseModel();
         ArrayList<RawItem> rawItemList = dbm.getRawItems();
+        ObservableList<RawItem> data = FXCollections.observableArrayList();
+        data.addAll(rawItemList);
 
-        ObservableList<ObservableList<String>> columnData = FXCollections.observableArrayList();
-
-        for(RawItem r : rawItemList)
-        {
-            ObservableList<String> row = FXCollections.observableArrayList();
-            row.add(r.getName());
-            row.add(r.getQuantity() + "");
-
-            columnData.add(row);
-        }
-
-        tableviewInventory.setItems(columnData);
+        tableviewInventory.setItems(data);
     }
 }
