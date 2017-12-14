37d22bd573510651935c851a636f88fa82955a1b
Patrick Sy <patrick.alq.sy@gmail.com>
2017-12-14 14:09:51 +0800

    Hook up Analytics buttons to screens
    Minor changes in InventoryController logic as well

--- src/controller/AnalyticsController.java
+++ src/controller/AnalyticsController.java
@@ -4,13 +4,18 @@ import controller.viewmanager.ViewManagerException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
 public class AnalyticsController extends Controller
 {
     @FXML
-    private Button buttonBackMenu;
+    private Button buttonXreading, buttonZreading, buttonSold, buttonWasted,
+            buttonBackMenu, buttonBackS, buttonBackW, buttonBackZ, buttonBackX;
+
+    @FXML
+    private AnchorPane analMainMenu, analMostSold, analMostWasted, analZReading, analXReading;
 
     public AnalyticsController(String fxmlpath, String csspath) throws IOException
     {
@@ -23,7 +28,45 @@ public class AnalyticsController extends Controller
         if(isFirstLoad())
         {
             buttonBackMenu.addEventHandler(ActionEvent.ACTION, e ->
-                viewManager.switchViews("MainMenuController"));
+                    viewManager.switchViews("MainMenuController"));
+
+            buttonXreading.addEventHandler(ActionEvent.ACTION, event ->
+            {
+                changePaneState("TO_XR");
+            });
+
+            buttonZreading.addEventHandler(ActionEvent.ACTION, event ->
+            {
+                changePaneState("TO_ZR");
+            });
+
+            buttonSold.addEventHandler(ActionEvent.ACTION, event ->
+            {
+                changePaneState("TO_MS");
+            });
+
+            buttonWasted.addEventHandler(ActionEvent.ACTION, event ->
+                    changePaneState("TO_MW"));
+
+            buttonBackX.addEventHandler(ActionEvent.ACTION, event ->
+            {
+                changePaneState("TO_MAIN");
+            });
+
+            buttonBackZ.addEventHandler(ActionEvent.ACTION, event ->
+            {
+                changePaneState("TO_MAIN");
+            });
+
+            buttonBackS.addEventHandler(ActionEvent.ACTION, event ->
+            {
+                changePaneState("TO_MAIN");
+            });
+
+            buttonBackW.addEventHandler(ActionEvent.ACTION, event ->
+            {
+                changePaneState("TO_MAIN");
+            });
         }
     }
 
@@ -31,5 +74,48 @@ public class AnalyticsController extends Controller
     public void clear()
     {
 
+    }
+
+    private void changePaneState(String state)
+    {
+        switch(state)
+        {
+            case "TO_XR":
+                analXReading.setVisible(true);
+                analXReading.setDisable(false);
+                analMainMenu.setVisible(false);
+                analMainMenu.setDisable(true);
+                break;
+            case "TO_ZR":
+                analZReading.setVisible(true);
+                analZReading.setDisable(false);
+                analMainMenu.setVisible(false);
+                analMainMenu.setDisable(true);
+                break;
+            case "TO_MS":
+                analMostSold.setVisible(true);
+                analMostSold.setDisable(false);
+                analMainMenu.setVisible(false);
+                analMainMenu.setDisable(true);
+                break;
+            case "TO_MW":
+                analMostWasted.setVisible(true);
+                analMostWasted.setDisable(false);
+                analMainMenu.setVisible(false);
+                analMainMenu.setDisable(true);
+                break;
+            case "TO_MAIN":
+                analMainMenu.setVisible(true);
+                analMainMenu.setDisable(false);
+                analMostSold.setVisible(false);
+                analMostSold.setDisable(true);
+                analMostWasted.setVisible(false);
+                analMostWasted.setDisable(true);
+                analXReading.setVisible(false);
+                analXReading.setDisable(true);
+                analZReading.setVisible(false);
+                analZReading.setDisable(true);
+                break;
+        }
     }
 }
