957236f9bec4ec123faa92434db1210d150d1e6a
Gavin Sanchez <mark_gavin_sanchez@dlsu.edu.ph>
2017-12-15 11:35:23 +0800

    my Branch

--- src/controller/viewmanager/ViewManagerException.java
+++
@@ -1,9 +0,0 @@
-package controller.viewmanager;
-
-public class ViewManagerException extends Exception
-{
-    public ViewManagerException(String classname)
-    {
-        super("No ViewManager set in " + classname + " class. Initial load will not be executed.");
-    }
-}
